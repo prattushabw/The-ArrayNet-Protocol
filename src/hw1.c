cfd10566862bc3d21f3a3109ab5343c68d3e27ea
Prattusha <56740350+prattushabw@users.noreply.github.com>
2024-02-13 15:27:00 +0000

    push

--- src/hw1.c
+++ src/hw1.c
@@ -12,10 +12,10 @@ void print_packet_sf(unsigned char packet[]) {
     unsigned int source_port = packet[7] >> 4;
     unsigned int dest_port = packet[7] & 0x1F;
     unsigned int fragment_offset = (packet[8] << 8) + packet[9];
-    unsigned int packet_length = (packet[10] << 8) + packet[11];
-    unsigned int max_hop_count = packet[12] >> 3;
-    unsigned int checksum = (packet[12] & 0x07) << 20;
-    checksum += (packet[13] << 12) + (packet[14] << 4) + (packet[15] >> 4);
+    unsigned int packet_length = ((packet[9] & 0x03) << 12)+ (packet[10] << 4) + (packet[11] >> 4);
+    unsigned int max_hop_count = ((packet[11]& 0x0F) << 1)+(packet[12] >> 7);
+    unsigned int checksum = (packet[12] & 0x7F) << 16;
+    checksum += (packet[13] << 8) + (packet[14]); 
     unsigned int compression_scheme = ((packet[15] & 0xC0) >> 6);
     unsigned int traffic_class = packet[15] & 0x3F;
     
@@ -31,20 +31,43 @@ void print_packet_sf(unsigned char packet[]) {
     printf("Traffic Class: %u\n", traffic_class);
     
     // Payload
-    int payload_index = 17;
+    printf("Payload: ");
+    int payload_index = 16;
     while (payload_index < packet_length) {
         int payload_value = (packet[payload_index] << 24) + (packet[payload_index+1] << 16) +
         (packet[payload_index+2] << 8) + packet[payload_index+3];
-        printf("%d\n", payload_value);
+        printf("%d ", payload_value);
         payload_index += 4;
     }
-    printf("\n");
 }
 
 unsigned int compute_checksum_sf(unsigned char packet[])
 {
-    (void)packet;
-    return -1;
+    int divisor = (1 << 23) - 1;
+    unsigned int source_address = (packet[0] << 20) + (packet[1] << 12) + (packet[2] << 4) + (packet[3] >> 4);
+    unsigned int dest_address = ((packet[3] & 0x0F) << 24) + (packet[4] << 16) + (packet[5] << 8) + packet[6];
+    unsigned int source_port = packet[7] >> 4;
+    unsigned int dest_port = packet[7] & 0x1F;
+    unsigned int fragment_offset = (packet[8] << 8) + packet[9];
+    unsigned int packet_length = ((packet[9] & 0x03) << 12)+ (packet[10] << 4) + (packet[11] >> 4);
+    unsigned int max_hop_count = ((packet[11]& 0x0F) << 1)+(packet[12] >> 7);
+    
+    unsigned int compression_scheme = ((packet[15] & 0xC0) >> 6);
+    unsigned int traffic_class = packet[15] & 0x3F;
+    
+    unsigned long sum=source_address +dest_address+source_port+dest_port +fragment_offset+packet_length+max_hop_count+traffic_class; 
+    
+    // Payload
+ 
+    int payload_index = 16;
+    while (payload_index < packet_length) {
+        int payload_value = (packet[payload_index] << 24) + (packet[payload_index+1] << 16) +
+        (packet[payload_index+2] << 8) + packet[payload_index+3];
+        sum+=abs(payload_value);
+        payload_index += 4;
+    }
+    
+    return sum%divisor;
 }
 
 unsigned int reconstruct_array_sf(unsigned char *packets[], unsigned int packets_len, int *array, unsigned int array_len) {
